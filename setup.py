a9f038be51d00efb5b434c4bf27969a0732050f4
Rich Klee <24254625+richklee@users.noreply.github.com>
2022-10-10 16:28:59 +0100

    setup file

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name="alphasim",
+    version="0.0.1-alpha",
+    description="Minimalist backtester",
+    author="Rich Klee",
+    author_email="hello@richklee.xyz",
+    url="https://github.com/thecolngroup/alphasim",
+    py_modules=["backtest", "stats"],
+)
