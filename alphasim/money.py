5a78076fa16a944b681a92856fdbedc6bf5d8b94
Rich Klee <24254625+richklee@users.noreply.github.com>
2023-03-15 11:05:23 +0000

    sqrt profit money func

--- alphasim/money.py
+++ alphasim/money.py
@@ -1,6 +1,26 @@
+import math
+
+
 def initial_capital(initial: float, total: float) -> float:
+    """
+    Money is initial stake only.
+    No reinvestment of profits.
+    """
     return initial
 
 
 def total_equity(initial: float, total: float) -> float:
+    """
+    Money is reinvestment of all profits.
+    """
     return total
+
+
+def sqrt_profit(initial, total):
+    """
+    Money is a function of the sqrt of the capital growth rate.
+    Partial reinvestment of profits guards against likelihood of
+    increasingly severe drawdowns as equity grows.
+    See https://zorro-project.com/manual/en/tutorial_kelly.htm
+    """
+    return initial * math.sqrt(1 + (total - initial) / initial)
